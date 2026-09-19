2c9c5ed0c45ee224394e65db4664c609b42a0e27
DeepanshuSharma0705 <deephunter0705@gmail.com>
2026-09-20 01:21:29 +0530

    Custom api response and error handling

--- src/index.js
+++ src/index.js
@@ -8,7 +8,20 @@ dotenv.config({
   path: './env'
 })
 
+// asyanc fuction return some promises. when db is connected 
+
 connectDB()
+.then(() => {
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server is running at port : ${process.env.PORT}`)
+  })
+})
+.catch((err) => {
+  console.log("MONGO DB connection Field: ", err);
+  
+})
+
+
 
 /* 
 This is the method: 1 to connect Db 

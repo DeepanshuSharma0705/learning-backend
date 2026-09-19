c036a1065df4ef848436217a6989558bbceb0ff5
DeepanshuSharma0705 <deephunter0705@gmail.com>
2026-09-19 20:53:59 +0530

    Database connection/ set

---
+++ src/index.js
@@ -0,0 +1,37 @@
+
+import mongoose, { connect } from "mongoose";
+import { DB_NAME } from "./constants.js";
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: './env'
+})
+
+connectDB()
+
+/* 
+This is the method: 1 to connect Db 
+using async function for in case you db in other continent / other country
+
+import express from "express";
+const app = express();
+
+(async () => {
+  try{
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+    // we use this in case Db loaded properly but app is not able to listion
+    app.on("error", (error) => {
+      console.log("ERROR : " , error);
+      throw error;
+    })
+
+    app.listen(process.env.PORT, ()=>{
+      console.log(`App is listenig on port ${process.env.PORT}`);
+    })
+
+  }catch{
+    console.error("ERROR: ", error);
+    throw err
+  }
+})() */

c0bfaf2311cbb133e83ecb6055d6cc5ec85d8017
DeepanshuSharma0705 <deephunter0705@gmail.com>
2026-09-20 16:10:31 +0530

    some spelling correction

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -3,7 +3,7 @@ class ApiError extends Error{
     statuscode,
     message = "Something went wrong",
     error = [],
-    statck = "",
+    stack = "",
   ){
     super(message)
     this.statuscode = statuscode
@@ -13,7 +13,7 @@ class ApiError extends Error{
     this.errors = errors
 
     if(stack){
-      this.stack = statck
+      this.stack = stack
     }else{
       Error.captureStackTrace(this, this.constructor)
     }

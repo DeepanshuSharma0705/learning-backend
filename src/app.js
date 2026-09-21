9a587d4a15f9696ae6aeb1d0b89fde6dbf048aef
DeepanshuSharma0705 <deephunter0705@gmail.com>
2026-09-22 00:26:10 +0530

    first controller/ api testing / user register

--- src/app.js
+++ src/app.js
@@ -16,4 +16,11 @@ app.use(express.urlencoded({extended : true, limit :"16kb"}))
 app.use(express.static("public"))
 
 app.use(cookieParser())
+
+// routers import 
+import userRouter from "./routes/user.routes.js"
+
+
+// routers declaration
+app.use("/api/v1/users", userRouter)
 export {app};

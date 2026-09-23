0b48ace11c8c556e2d1052cb7504a4d9b8d5d171
DeepanshuSharma0705 <deephunter0705@gmail.com>
2026-09-23 21:29:10 +0530

    fix: correct variable name for refresh token validation in refreshAccessToken method

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -203,7 +203,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
   const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
 
   // if refersh token not present 
-  if (!refreshAccessToken) {
+  if (!incomingRefreshToken) {
     throw new ApiError(401, "Unauthorized request ");
   }
 
